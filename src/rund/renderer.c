bcadbd83312110d183bb0c08c87ac0c8183376e7
Bonfra04 <bonfract@gmail.com>
2021-10-29 20:54:22 +0200

    this is better

--- src/rund/renderer.c
+++ src/rund/renderer.c
@@ -148,7 +148,7 @@ draw_data_t draw_character(const buffer_t* buffer, color_t color, wchar_t charac
     draw_data_t data = NULL_DRAW;
     data.coords.x = transX;
 
-#define mandufra_number 0.0001
+#define mandufra_number 0.00001
     scale = (float)buffer->height * scale * mandufra_number; // idk why but this works
 #undef mandufra_number
 
